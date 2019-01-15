b8d5a0f4fed1f7bafe9134b8e980276d3c81f9b0
masterUNG <phrombutr@gmail.com>
2019-01-15 10:55:53 +0700

    Print Success

---
+++ ArrayAndDictionary.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var numbers = [11,22,33,44]
+var names = ["AAA", "BBB", "CCC"]
+
+numbers[3]
+
+//การเพิ่ม สมาชิค Array
+numbers.append(50)
+names.append("Doramon")
+
+//Delete Member of Array
+numbers
+numbers.remove(at: 1)
+numbers
+
+//การหา ขนาดของ Array
+print("Test Print")
+names
+print("names[3] ==> " + names[3])
+print("names[3] ==> \(names[3])")
