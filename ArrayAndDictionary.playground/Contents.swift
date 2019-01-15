7fa1e3d9a18c3fa45775a544260f9359dc4fcad4
masterUNG <phrombutr@gmail.com>
2019-01-15 11:12:30 +0700

    Dictionary Success

--- ArrayAndDictionary.playground/Contents.swift
+++ ArrayAndDictionary.playground/Contents.swift
@@ -3,6 +3,7 @@ import UIKit
 var numbers = [11,22,33,44]
 var names = ["AAA", "BBB", "CCC"]
 
+//About Array
 numbers[3]
 
 //การเพิ่ม สมาชิค Array
@@ -19,3 +20,23 @@ print("Test Print")
 names
 print("names[3] ==> " + names[3])
 print("names[3] ==> \(names[3])")
+
+let lenghtNumber: Int = numbers.count
+print("lenghtNumber = \(lenghtNumber)")
+
+//About Dictionary
+var friend = ["key1": "Doramon", "key2": "Nopita", "key3": "Sunaki", "key4": "MasterUNG"]
+print("friend ที่มีค่า key เท่ากับ key1 ==> \(friend["key1"]!)")
+
+let lenghtFriend = friend.count
+print("ขนาดของ Dictinary ==> \(lenghtFriend)")
+
+//Add Member to Dictionary
+friend["extraFriend"] = "CherParng"
+print("freind ==>> \(friend)")
+
+//Delete Member Dicionary
+friend.removeValue(forKey: "key1")
+print("freind ==>> \(friend)")
+
+
