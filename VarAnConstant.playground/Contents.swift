15a38d09759c4c74204394e04adbd30714cfac3f
masterUNG <phrombutr@gmail.com>
2019-01-15 09:38:09 +0700

    Var Success

---
+++ VarAnConstant.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//การประกาศ ตัวแปร และ ค่าคงที่
+//Implicit Variable คือ การประกาศตัวแปรที่ เปลี่ยนแปลงค่าได้ และ ใช้ Data Type จาก ค่าเริ่มต้นมากำหนด ชนิดตัวแปร
+var nameString = "MasterUNG"
+
+nameString = "Doramon"
+
+var number1 = 123
+
+//Explicit Variable
+var surName: String = "Phrombutr"
+var number2: Int = 555
+var number3: Double = 12.34
+var status1: Bool = true
+
+
+
+
+
+
+
+
+
